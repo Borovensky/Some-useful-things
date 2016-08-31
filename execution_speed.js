509c8abba8d16071a0bd342a635b3850003ff9bd
Artem <borovenskyartem.gmail.com>
2016-08-31 03:18:22 +0300

    update

---
+++ execution_speed.js
@@ -0,0 +1,9 @@
+/* Here an example how we can check the 
+	speed of execution code */
+var start_time = new Date().getTime();
+
+	// code here
+
+var end_time = new Date().getTime();
+
+console.log('execution time:', end_time - start_time);
